f7058ea4bc62b74febd564998ae3ad7a454c5f0f
danootbenoot <47220527+danootbenoot@users.noreply.github.com>
2019-05-17 11:36:56 -0400

    Add files via upload

---
+++ Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	public int score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "" + score;	
+
+
+		if (score >= 1200) {
+			SceneManager.LoadScene ("Win");
+		}
+	}
+
+	public void IncreaseScore (int amountToIncrease){
+		score += amountToIncrease;
+	}
+}
